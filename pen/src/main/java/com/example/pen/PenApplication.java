4c03391d82899304fdfddee9c7df2c9c944c9020
kartik <kartikraut12112000@gmail.com>
2024-03-26 23:03:14 +0530

    First commit

---
+++ pen/src/main/java/com/example/pen/PenApplication.java
@@ -0,0 +1,13 @@
+package com.example.pen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenApplication.class, args);
+	}
+
+}
